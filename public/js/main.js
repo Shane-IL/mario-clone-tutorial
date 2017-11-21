24758b3feba4428c8ff4092dba2a7f1d79753139
Shane <shanegoodson101@gmail.com>
2017-11-21 18:56:39 +0200

    Episode 8: fixed key control issue, removed some debugging code, set camera to move with mario

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,7 @@ import Camera from './Camera.js';
 import Timer from './Timer.js';
 import { loadLevel } from './loaders.js';
 import { createMario } from './entities.js';
-import { createCollisionLayer, createCameraLayer } from './layers.js';
 import { setupKeyboard } from './input.js';
-import { setupMouseControl } from './debug.js';
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext('2d');
@@ -21,22 +19,19 @@ Promise.all([
 
         level.entities.add(mario);
 
-        // level.comp.layers.push(
-        //     createCollisionLayer(level),
-        //     createCameraLayer(camera)
-        // );
-
         mario.pos.set(64, 64);
 
         const input = setupKeyboard(mario);
 
         input.listenTo(window);
-
-        //Debug
-        setupMouseControl(canvas, mario, camera);
-
+    
         timer.update = function update(deltaTime) {
             level.update(deltaTime);
+
+            if(mario.pos.x > 100) {
+               camera.pos.x = mario.pos.x - 100; 
+            }
+
             level.comp.draw(context, camera);
         }
         timer.start();
