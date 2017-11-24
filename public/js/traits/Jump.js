2f73639ef33deb35e1aff126fd59ab7d26dbc989
Shane <shanegoodson101@gmail.com>
2017-11-24 16:41:22 +0200

    Episode 8 - Added obstruction and handled jump with it

--- public/js/traits/Jump.js
+++ public/js/traits/Jump.js
@@ -3,17 +3,26 @@ import { Trait } from '../Entity.js';
 export default class Jump extends Trait {
 	constructor() {
 		super('jump');
+		this.ready = false;
 		this.duration = 0.5;
 		this.velocity = 200;
 		this.engageTime = 0;
 	}
 
 	start() {
-		this.engageTime = this.duration;
+		if(this.ready) {
+			this.engageTime = this.duration;
+		}
 	}
 
 	cancel() {
 		this.engageTime = 0;
+	}
+
+	obstruct(entity, side) {
+		if(side === 'bottom') {
+			this.ready = true;
+		}
 	}
 
 	update(entity, deltaTime) {
@@ -21,5 +30,6 @@ export default class Jump extends Trait {
 			entity.vel.y = -this.velocity;
 			this.engageTime -= deltaTime;
 		}
+		this.ready = false;
 	}
 }
