1d4b0baa02f431c35874308698ac80c4aad75e04
Shane <shanegoodson101@gmail.com>
2017-12-16 00:23:17 +0200

    Episode 9 - Decoupled level loaders into specialized file

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,9 +1,7 @@
-import Level from './Level.js';
 import SpriteSheet from './SpriteSheet.js';
 import { createAnimation } from './animation.js';
-import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 
-function loadJSON(url) {
+export function loadJSON(url) {
 	return fetch(url)
 		.then(r => r.json());
 }
@@ -49,62 +47,3 @@ export function loadSpriteSheet(name) {
 			return sprites;
 		});
 }
-
-export function loadLevel(name) {
-	return loadJSON(`/levels/${name}.json`)
-		.then(levelSpec => Promise.all([
-			levelSpec,
-			loadSpriteSheet(levelSpec.spriteSheet)
-		]))
-		.then(([levelSpec, backgroundSprites]) => {
-			const level = new Level();
-
-			createTiles(level, levelSpec.tiles, levelSpec.patterns);
-
-			const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
-			const spriteLayer = createSpriteLayer(level.entities);
-
-			level.comp.layers.push(backgroundLayer);
-			level.comp.layers.push(spriteLayer);
-
-			return level;
-		});
-}
-
-export function createTiles(level, tiles, patterns, offsetX = 0, offsetY = 0) {
-	function applyRange(tile, xStart, xLength, yStart, yLength) {
-		const xEnd = xStart + xLength;
-		const yEnd = yStart + yLength;
-		for (let x = xStart; x < xEnd; ++x) {
-			for (let y = yStart; y < yEnd; ++y) {
-				const derivedX = x + offsetX;
-				const derivedY = y + offsetY;
-				if (tile.pattern) {
-					const tiles = patterns[tile.pattern].tiles;
-					createTiles(level, tiles, patterns, derivedX, derivedY);
-				} else {
-					level.tiles.set(derivedX, derivedY, {
-						name: tile.name,
-						type: tile.type
-					});
-				}
-			}
-		}
-	}
-
-	tiles.forEach(tile => {
-		tile.ranges.forEach(range => {
-			if (range.length === 4) {
-				const [xStart, xLength, yStart, yLength] = range;
-				applyRange(tile, xStart, xLength, yStart, yLength);
-			} else if (range.length === 3) {
-				const [xStart, xLength, yStart] = range;
-				applyRange(tile, xStart, xLength, yStart, 1);
-			} else if (range.length === 2) {
-				const [xStart, yStart] = range;
-				applyRange(tile, xStart, 1, yStart, 1);
-			}
-		});
-	});
-}
-
