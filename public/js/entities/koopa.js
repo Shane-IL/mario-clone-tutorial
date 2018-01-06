74703f9b3af20f7a30e6f4ffaee094ecc9d448c2
Shane <shanegoodson101@gmail.com>
2018-01-06 13:17:31 +0200

    Ep 12 - Made koopa killable

--- public/js/entities/koopa.js
+++ public/js/entities/koopa.js
@@ -1,10 +1,34 @@
-import Entity from '../Entity.js';
+import Entity, {Sides, Trait} from '../Entity.js';
+import Killable from '../traits/Killable.js';
 import PendulumWalk from '../traits/PendulumWalk.js';
 import { loadSpriteSheet } from '../loaders.js';
 
 export function loadKoopa() {
 	return loadSpriteSheet('koopa')
 		.then(createKoopaFactory);
+}
+
+class Behavior extends Trait {
+	constructor() {
+		super('behavior');
+	}
+
+	collides(us, them) {
+		if (us.killable.dead) {
+			return;
+		}
+
+		if (them.stomper) {
+			if (them.vel.y > us.vel.y) {
+				us.killable.kill();
+				them.stomper.bounce();
+				us.pendulumWalk.speed = 0;
+			} else {
+				them.killable.kill();
+			}
+			
+		}
+	}
 }
 
 function createKoopaFactory(sprite) {
@@ -19,6 +43,8 @@ function createKoopaFactory(sprite) {
 		koopa.offset.y = 8;
 
 		koopa.addTrait(new PendulumWalk());
+		koopa.addTrait(new Behavior());
+		koopa.addTrait(new Killable());
 
 		koopa.draw = drawKoopa;
 
