3ce398f76db0299111b0ac9c41b075211aa0885d
Shane <shanegoodson101@gmail.com>
2018-02-01 22:07:24 +0200

    Ep 14 - Added dashboard layer

---
+++ public/js/loaders/font.js
@@ -0,0 +1,33 @@
+import { loadImage } from '../loaders.js';
+import SpriteSheet from '../SpriteSheet.js';
+
+const CHARS = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+
+class Font {
+	constructor(sprites, size) {
+		this.sprites = sprites;
+		this.size = size;
+	}
+
+	print(text, context, x, y) {
+		[...text].forEach((char, pos) => {
+			this.sprites.draw(char, context, x + pos * this.size, y);
+		});
+	}
+}
+
+export function loadFont() {
+	return loadImage('./img/font.png')
+		.then(image => {
+			const fontSprite = new SpriteSheet(image);
+			const size = 8;
+			const rowLength = image.width;
+
+			for (const [index, char] of [...CHARS].entries()) {
+				const x = index * size % rowLength;
+				const y =  Math.floor(index * size / rowLength) * size;
+				fontSprite.define(char, x, y, size, size);
+			}
+			return new Font(fontSprite, size);
+		});
+}
